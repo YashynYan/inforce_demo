1d8dec3d9abea6bc1ded71e1da6097a4e042f604
Yan Yashyn <yyashyn001@cee.ema.ad.pwcinternal.com>
2020-11-08 19:10:48 +0200

    added region name to region state table

--- src/components/feedbacker-region-stat/RegionStatTable.js
+++ src/components/feedbacker-region-stat/RegionStatTable.js
@@ -1,21 +1,24 @@
 import React, { useEffect }  from 'react'
 import {connect, useDispatch} from 'react-redux'
+import { fetchRegions } from '../../redux/actions/formActions'
 import { feedbacksByRegionId } from '../../redux/actions/StateTablesActions'
 
 function RegionStatTable(props) {
     console.log(props)
-    const {match, citiesTableArray} = props
+    const {match, citiesTableArray, regions} = props
     const regionId = match.params.id
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(feedbacksByRegionId(regionId))
+        dispatch(fetchRegions())
     }, [])
 
-
+    const region = regions.find(region => region.id===parseInt(regionId))
+    
     return (
         <div>
-            <h2>Распределение по городам</h2>
+            <h2>Распределение по городам региона {region? `"${region.name}"`:null }</h2>
             <table>
                 <thead>
                     <tr>
@@ -44,7 +47,8 @@ function RegionStatTable(props) {
 const mapStateToProps = state => {
     console.log(state)
     return { 
-        citiesTableArray: state.tableReducer.tableOfCities
+        citiesTableArray: state.tableReducer.tableOfCities,
+        regions: state.formReducer.regions
     }
     }
 
